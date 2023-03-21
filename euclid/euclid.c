c7537b2e57bafef3f765aa4107717ff703a3c0c2
user <user@DESKTOP-G5TOH8T>
2023-03-21 13:13:10 +0900

    초기 커밋.

---
+++ euclid/euclid.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int euclid(int a, int b)
+{
+    // If (b=0) return a
+    if (b == 0) return a;
+    // return Euclid(b, a mod b)
+    return euclid(b, a % b);
+}
+
+int test_euclid()
+{
+    int gcd = 0;
+
+    gcd = euclid(34, 48);
+    printf("Hello World! %d \n", gcd);
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    test_euclid();
+}
