fcf350f7468b3af6064489a086792da1dd1fec32
m9310142 <m9310142@naver.com>
2023-03-30 18:34:32 +0900

    findFakecoin

--- chap1/chap1/findFakeCoin.c
+++ chap1/chap1/findFakeCoin.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 
-int find_FakeCoin() {
-    int coins[10] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 };            // 진짜 동전 9개와 가짜 동전 1개
-    int n = 10;                                                  // 전체 동전의 수
-    int dummy1 = 0, dummy2 = n - 1;                              // 동전을 나눌 왼쪽과 오른쪽 인덱스
-    int mid = (dummy1 + dummy2) / 2;                             // 중앙값을 pivot으로 설정
+void findFakeCoin(int* coin, int left, int right);
 
-    while (dummy2 >= dummy1) {                                   // dummy1이 dummy2보다 작거나 같은 동안 반복
-        if (coins[mid] == 0) {                                   // mid 동전이 가짜 동전인 경우
-            printf("가짜 동전은 %d번째 동전입니다.", mid + 1);   // mid 위치 출력
-            break;
+void findFakeCoin(int* coin, int left, int right){
+
+    int count = 0;
+    int sum1, sum2;
+    int mid;
+    whilt(left < right) 
+        count++;
+        sum1 = 0;
+        sum2 = 0;
+        mid = (left + right) / 2;
+        for (int i = left; i <= right; i++) {
+            if (i <= mid) sum1 += coin[i];
+            else sum2 += coin[i];
         }
-        else if (coins[mid] == 1) {                              // mid 동전이 진짜 동전인 경우
-            dummy1 = mid + 1;                                    // 왼쪽 부분집합 제거
+        if (sum1 < sum2) {
+            right = mid;
         }
-        else {                                                   // mid 동전이 없는 경우
-            dummy2 = mid - 1;                                    // 오른쪽 부분집합 제거
+        else {
+            left = mid + 1;
         }
-        mid = (dummy1 + dummy2) / 2;                             // 새로운 mid 계산
     }
-    return 0;
 }
+
 
 int main() {
     find_FakeCoin();
---
+++ chap1/findFakeCoin/findFakeCoin.c
@@ -0,0 +1,60 @@
+﻿#include <stdio.h>
+
+
+int findFakeCoin(int coins[], int start, int end, int weight) {
+    if (start == end) { // 검색 범위가 1개인 경우
+        if (coins[start] == weight) {
+            return -1; // 모든 동전이 진짜인 경우
+        }
+        else {
+            return start; // 가짜 동전의 위치를 반환
+        }
+    }
+
+    int mid = (start + end) / 2; // 검색 범위를 둘로 나누는 중간 인덱스
+    int left_sum = 0, right_sum = 0;
+
+    // 검색 범위를 둘로 나누어 각각의 합을 구함
+    for (int i = start; i <= mid; i++) {
+        left_sum += coins[i];
+    }
+    for (int i = mid + 1; i <= end; i++) {
+        right_sum += coins[i];
+    }
+
+    if (left_sum < right_sum) { // 오른쪽에 가짜 동전이 있는 경우
+        return findFakeCoin(coins, mid + 1, end, weight);
+    }
+    else if (left_sum > right_sum) { // 왼쪽에 가짜 동전이 있는 경우
+        return findFakeCoin(coins, start, mid, weight);
+    }
+    else { // 두 부분합이 같은 경우
+        // 중간 인덱스가 참조 동전의 위치인 경우
+        if (mid == weight) {
+            return mid + 1; // 오른쪽에 가짜 동전이 있는 것으로 처리
+        }
+        else if (mid < weight) { // 참조 동전이 더 오른쪽에 있는 경우
+            return findFakeCoin(coins, mid + 1, end, weight);
+        }
+        else { // 참조 동전이 더 왼쪽에 있는 경우
+            return findFakeCoin(coins, start, mid, weight);
+        }
+    }
+}
+
+int main() {
+    int coins[10] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1 }; // 10개의 동전 배열 (9개는 진짜, 1개는 가짜)
+    int n = 10; // 배열의 크기
+    int weight = 6; // 참조 동전의 무게
+
+    int fake_coin = findFakeCoin(coins, 0, n - 1, weight); // 가짜 동전의 인덱스 검색
+
+    if (fake_coin == -1) {
+        printf("모든 동전이 진짜입니다.\n");
+    }
+    else {
+        printf("%d번째 동전이 가짜입니다.\n", fake_coin + 1);
+    }
+
+    return 0;
+}
